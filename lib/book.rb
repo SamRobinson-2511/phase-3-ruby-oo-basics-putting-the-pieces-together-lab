b630d77dc301d95463a534e5418ab4ecdc33ca8d
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2022-12-14 12:27:54 -0600

    commit tree dbaffffc47870675ec3825034c56523466bc4a7a

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
+
+    def initialize(title = "And Then There Were None", author="Agatha Christie", page_count=272, genre="Mystery")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+        @turn_page = turn_page
+    end
+end
+
+book = Book.new
+
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition, :cobble
+
+    def initialize(brand="Nike", color="Black", size=15, material="leather", condition="bad")
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    
+    def cobble(condition)
+        if condition == "bad"
+            puts "shoe has been repaired"
+            condition = "new"
+        end
+    end
+
+
+    end
+end
+
+s1 = Shoe.new
